509a44d6241b6fd160b6c7521564e109ba63c9d9
L-Timothy <2334375990@qq.com>
2024-05-19 16:12:56 +0800

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,24 @@ function searchTable() {
         }
     }
 }
+
+function toggleDropdown() {
+    var dropdownContent = document.getElementById("dropdown-content");
+    if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+    } else {
+        dropdownContent.style.display = "block";
+    }
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.style.display === "block") {
+                openDropdown.style.display = "none";
+            }
+        }
+    }
+}
